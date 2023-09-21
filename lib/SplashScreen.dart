1f8116af1492d9e67af88e36836eb97ddcaa30bc
luizeduardom <luizeduardocffg@gmail.com>
2023-09-21 09:24:49 -0300

    projeto dividido em classes e adicionado splashscreen ao entrar no app

---
+++ lib/SplashScreen.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+import 'LoginScreen.dart';
+class SplashScreen extends StatefulWidget {
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  bool _visible = true;
+
+  @override
+  void initState() {
+    super.initState();
+    // Após um atraso, inicie a animação de fade
+    Future.delayed(const Duration(seconds: 4), () {
+      setState(() {
+        _visible = false;
+      });
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          const LoginScreen(),
+
+          // Tela de Abertura com animação de fade
+          AnimatedOpacity(
+            opacity: _visible ? 1.0 : 0.0,
+            duration: const Duration(seconds: 1),
+            child: const Center(
+              child: CircularProgressIndicator(
+                color: Colors.white,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,122 +1,19 @@
 import 'package:flutter/material.dart';
 
+import 'SplashScreen.dart';
+
 void main() {
-  runApp(const LoginApp());
+  runApp(const TrainingSheep());
 }
 
-class LoginApp extends StatelessWidget {
-  const LoginApp({super.key});
+class TrainingSheep extends StatelessWidget {
+  const TrainingSheep({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: LoginPage(),
+    return MaterialApp(
+      home: SplashScreen(),
       debugShowCheckedModeBanner: false,
-    );
-  }
-}
-
-class LoginPage extends StatelessWidget {
-  const LoginPage({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      resizeToAvoidBottomInset: false,
-      body: Container(
-        padding: const EdgeInsets.all(20.0),
-        color: Colors.black, // Fundo preto
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Container(
-                width: 200.0,
-                // Largura desejada para a imagem
-                height: 200.0,
-                margin: const EdgeInsets.only(bottom: 82),
-                // Altura desejada para a imagem
-                child: Image.asset('assets/icon/login.png'),
-              ),
-              const CustomTextField(labelText: 'Usuário'),
-              const SizedBox(height: 20.0),
-              const CustomTextField(labelText: 'Senha', obscureText: true),
-              const SizedBox(height: 10.0),
-              Align(
-                alignment: Alignment.centerRight,
-                child: TextButton(
-                  onPressed: () {
-                    // lógica para a ação "Esqueci minha senha" aqui
-                  },
-                  child: const Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      InkWell(
-                        child: Text(
-                          'Esqueci minha senha',
-                          style: TextStyle(color: Colors.white, fontSize: 16),
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-              const SizedBox(height: 75.0),
-              ElevatedButton(
-                  onPressed: () {
-                    // Implemente a lógica para o botão "Entrar" aqui
-                  },
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.blueAccent,
-                    minimumSize: const Size(150, 50),
-                  ),
-                  child: const Text(
-                    'Entrar',
-                    style: TextStyle(fontSize: 20),
-                  )),
-              const SizedBox(height: 30.0),
-              Container(
-                margin: const EdgeInsetsDirectional.only(top: 30),
-                child: const InkWell(
-                  child: Text(
-                    'Registrar',
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 21,
-                    ),
-                  ),
-                ),
-              ),
-            ],
-          ),
-        ),
-      ),
-    );
-  }
-}
-
-class CustomTextField extends StatelessWidget {
-  final String labelText;
-  final bool obscureText;
-
-  const CustomTextField(
-      {super.key, required this.labelText, this.obscureText = false});
-
-  @override
-  Widget build(BuildContext context) {
-    return TextFormField(
-      obscureText: obscureText,
-      style: const TextStyle(color: Colors.white),
-      decoration: InputDecoration(
-        labelText: labelText,
-        labelStyle: const TextStyle(color: Colors.white),
-        focusedBorder: const OutlineInputBorder(
-          borderSide: BorderSide(color: Colors.white70),
-        ),
-        enabledBorder: const OutlineInputBorder(
-          borderSide: BorderSide(color: Colors.white70),
-        ),
-      ),
     );
   }
 }
